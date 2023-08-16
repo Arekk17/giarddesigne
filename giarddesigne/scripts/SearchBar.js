24587e1f37120d55cac7428828ad2dce06a14613
Arek Daszkowski <daszkowskiarek145@gmail.com>
2023-08-17 00:51:18 +0200

    fixed naviation

--- giarddesigne/scripts/SearchBar.js
+++ giarddesigne/scripts/SearchBar.js
@@ -1,20 +1,10 @@
-var searchInput = document.getElementById("searchInput");
-var searchIcon = document.getElementById("searchIcon");
-var searchInputGroup = document.querySelector(".search-input");
-var navItems = document.querySelectorAll(".nav-item");
+const searchBox = document.querySelector(".searchBox");
+const searchBtn = document.querySelector(".search");
+const closeBtn = document.querySelector(".close");
 
-searchIcon.addEventListener("click", function () {
-  searchInputGroup.classList.toggle("active");
-  searchInput.style.display = searchInputGroup.classList.contains("active")
-    ? "block"
-    : "none";
-  navItems.forEach(function (navItem) {
-    if (navItem !== searchIcon.parentElement) {
-      navItem.classList.toggle("active");
-    }
-  });
-
-  if (searchInputGroup.classList.contains("active")) {
-    searchInput.focus();
-  }
+searchBtn.addEventListener("click", () => {
+  searchBox.classList.add("active");
+});
+closeBtn.addEventListener("click", () => {
+  searchBox.classList.remove("active");
 });
