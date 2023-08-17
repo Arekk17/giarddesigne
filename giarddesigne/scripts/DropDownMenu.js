a0a45598d8c6c547aefafbd1dda17f672ce8825c
Arek Daszkowski <daszkowskiarek145@gmail.com>
2023-08-17 16:58:29 +0200

    delete comments

--- giarddesigne/scripts/DropDownMenu.js
+++ giarddesigne/scripts/DropDownMenu.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
-  // Obsługa kliknięcia na element oferty
   $(".nav-item.dropdown > a").on("click", function (e) {
-    e.preventDefault(); // Zapobiegaj domyślnemu zachowaniu linku
+    e.preventDefault();
     var dropdownMenu = $(this).parent().find(".dropdown-menu");
-    $(".dropdown-menu").not(dropdownMenu).slideUp(200); // Schowaj inne otwarte menu
+    $(".dropdown-menu").not(dropdownMenu).slideUp(200);
     dropdownMenu.slideToggle(200);
   });
 
-  // Obsługa kliknięcia na inne miejsce na stronie
   $(document).on("click", function (e) {
     if (!$(e.target).closest(".nav-item.dropdown").length) {
       $(".dropdown-menu").slideUp(200);
