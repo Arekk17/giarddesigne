32c7ec7374fb5ec6bbe3c726b89e31c2fc93cb27
Arek Daszkowski <daszkowskiarek145@gmail.com>
2023-08-22 01:49:03 +0200

    move files to src. add vite config

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import "../scripts/AboutCompany";
+import "../scripts/DropDownMenu";
+import "../scripts/Gradient";
+import "../scripts/MasnoryGallery";
+import "../scripts/OfferSquareAnimation";
+import "../scripts/Popup";
+import "../scripts/SearchBar";
